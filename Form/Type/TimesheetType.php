90b9e8a98f48a059d37d5f574e7518a1ecad0ab1
root <root@hamdi-OptiPlex-390.(none)>
2014-05-21 12:24:35 +0100

    test

--- Form/Type/TimesheetType.php
+++ Form/Type/TimesheetType.php
@@ -8,4 +8,24 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class TimesheetType extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('project')
+            ->add('timeSpent')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Karis\TimesheetBundle\Entity\Timesheet'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'karis_timesheetbundle_timesheet';
+    }
 }
